271cf4ce5618be5c248af4829ce107fc0a1667b6
Faraz <faraz.yaqoob@gmail.com>
2026-04-13 21:57:55 -0700

    Day 1 updates

--- python-basics/day1.py
+++ python-basics/day1.py
@@ -1,1 +1,13 @@
 print("Day 1 - AI Engineer Journey Started")
+
+name = "Faraz"
+age = 47
+city = "San Leandro"
+weight = 200
+goal_weight = 180
+height_ft = 6
+
+height_in = height_ft * 12
+
+bmr = (4.54 * weight) + (15.88 * height_in) - (5 * age) + 5
+print("BMR is ", bmr)
